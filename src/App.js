e17775079a62458f64e05f8c2a973445a743ad0b
Nishita Gupta <g.nishi9525@gmail.com>
2023-01-10 18:55:47 +0530

    image changed

--- src/App.js
+++ src/App.js
@@ -7,47 +7,46 @@ function App() {
   const botmessage = useRef();
   const input = useRef();
 
-  const date = new Date();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
-  const day = date.getDay();
-  const month = date.getMonth();
-  const year = date.getFullYear();
+  // const date = new Date();
+  // const hours = date.getHours();
+  // const seconds = date.getSeconds();
+  // const day = date.getDay();
+  // const month = date.getMonth();
+  // const year = date.getFullYear();
 
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const [time, setTime] = useState(`${hours}:${seconds}`); //using the useState hook to get the data from the local time and set it to the time variable
-  const [dateTime, setDateTime] = useState(
-    `${days[day]}, ${months[month]} ${year}`
-  ); //using the useState hook to get the data from the local date and set it to the dateTime variable
+  // const days = [
+  //   "Sunday",
+  //   "Monday",
+  //   "Tuesday",
+  //   "Wednesday",
+  //   "Thursday",
+  //   "Friday",
+  //   "Saturday",
+  // ];
+  // const months = [
+  //   "January",
+  //   "February",
+  //   "March",
+  //   "April",
+  //   "May",
+  //   "June",
+  //   "July",
+  //   "August",
+  //   "September",
+  //   "October",
+  //   "November",
+  //   "December",
+  // ];
+  // const [time, setTime] = useState(`${hours}:${seconds}`); //using the useState hook to get the data from the local time and set it to the time variable
+  // const [dateTime, setDateTime] = useState(`${days[day]}, ${months[month]} ${year}`);
+  //using the useState hook to get the data from the local date and set it to the dateTime variable
 
   const checkStatus = (e) => {
     let isActive = true;
-    if (dateTime === "Thursday, Aug 13 2022") {
-      //if the dateTime is Thursday, 13 Aug 2022, the bot will be inactive
-      isActive = false;
-    }
+    // if (dateTime === "Thursday, Aug 13 2022") {
+    //   //if the dateTime is Thursday, 13 Aug 2022, the bot will be inactive
+    //   isActive = false;
+    // }
     const status = document.querySelector(".status");
     // selecting the status class
     if (isActive === true) {
@@ -60,13 +59,28 @@ function App() {
     }
   };
   const handleInput = () => {
-    const botMessage = document.querySelector("#message1");
-    const userMessage = document.querySelector("#message2");
+    // const botMessage = document.querySelector("#message1");
+    // const userMessage = document.querySelector("#message2");
     const inputRef = input.current;
     const getHumanMessage = humanMessage.current;
     const getBotMessage = botmessage.current;
 
     let badwords = ["fuck|bad|stupid|useless|bitch|crazy|nonsense"];
+    let welcome = ["hi|hello|Hello|hey|sup|yo|wassup|whats up|howdy|greetings|good morning|good afternoon|good evening",];
+    let bye = ["bye|Bye|goodbye|see you later|cya|goodnight|goodbye|bie"];
+    let thanks = ["Thanks|thanks|thank you|thank you very much|Thank you very much",];
+    let how = ["How are you|how are you doing|how are you doing today|how are you doing today|How are you|how are you",];
+    let good = ["That's good|Sound nice|that sounds awesome|that sounds great|Great|great|sounds great|that's sounds good|Nice|nice",];
+    let response = ["I'm fine|I am fine|I am fine today|I am fine today|i'm fine|i'm great|I'm fine|I'm great|I'm good|i'm good|great|Great",];
+    let nameAsk = ["What's your name|what's your name|What is your name|what is your name",];
+    let owner = ["Who is the owner|who is the owner|Who is the owner of this bot|who is the owner of this bot|Who made you|who made you|Who is your maker|Who made you|who is your maker|who is your owner|Who is your owner",];
+    let owner2 = ["Who's Treasure|who's Treasure|Who is Treasure|who is Treasure",];
+    let ageAsk = ["What's your age|what's your age|What is your age|what is your age|How old are you|how old are you",];
+
+
+
+
+
     let words = new RegExp(badwords);
     if (words.test(document.querySelector("#input").value)) {
       // if the input contains bad words
@@ -76,9 +90,7 @@ function App() {
         inputRef.value = ""; // clear the input
       }, 2000);
     }
-    let welcome = [
-      "hi|hello|Hello|hey|sup|yo|wassup|whats up|howdy|greetings|good morning|good afternoon|good evening",
-    ];
+
     let words2 = new RegExp(welcome);
     if (words2.test(document.querySelector("#input").value)) {
       const status = document.querySelector(".status");
@@ -91,7 +103,7 @@ function App() {
         inputRef.value = ""; // clear the input
       }, 2000);
     }
-    let bye = ["bye|Bye|goodbye|see you later|cya|goodnight|goodbye"];
+
     let words3 = new RegExp(bye);
     if (words3.test(document.querySelector("#input").value)) {
       const status = document.querySelector(".status");
@@ -105,9 +117,7 @@ function App() {
         status.style.color = "red";
       }, 5000);
     }
-    let thanks = [
-      "Thanks|thanks|thank you|thank you very much|Thank you very much",
-    ];
+
     let words4 = new RegExp(thanks);
     if (words4.test(document.querySelector("#input").value)) {
       getBotMessage.innerText = "Typing...";
@@ -116,9 +126,7 @@ function App() {
         inputRef.value = ""; // clear the input
       }, 2000);
     }
-    let how = [
-      "How are you|how are you doing|how are you doing today|how are you doing today|How are you|how are you",
-    ];
+
     let words5 = new RegExp(how);
     if (words5.test(document.querySelector("#input").value)) {
       const status = document.querySelector(".status");
@@ -130,9 +138,7 @@ function App() {
         inputRef.value = ""; // clear the input
       }, 2000);
     }
-    let good = [
-      "That's good|Sound nice|that sounds awesome|that sounds great|Great|great|sounds great|that's sounds good|Nice|nice",
-    ];
+
     let words6 = new RegExp(good);
     if (words6.test(document.querySelector("#input").value)) {
       getBotMessage.innerText = "Typing...";
@@ -142,9 +148,6 @@ function App() {
       }, 1000);
     }
 
-    let response = [
-      "I'm fine|I am fine|I am fine today|I am fine today|i'm fine|i'm great|I'm fine|I'm great|I'm good|i'm good|great|Great",
-    ];
     let words7 = new RegExp(response);
     if (words7.test(document.querySelector("#input").value)) {
       getBotMessage.innerText = "Typing...";
@@ -154,9 +157,6 @@ function App() {
       }, 2000);
     }
 
-    let nameAsk = [
-      "What's your name|what's your name|What is your name|what is your name",
-    ];
     let words8 = new RegExp(nameAsk);
     if (words8.test(document.querySelector("#input").value)) {
       getBotMessage.innerText = "Typing...";
@@ -166,9 +166,6 @@ function App() {
       }, 2000);
     }
 
-    let owner = [
-      "Who is the owner|who is the owner|Who is the owner of this bot|who is the owner of this bot|Who made you|who made you|Who is your maker|Who made you|who is your maker|who is your owner|Who is your owner",
-    ];
     let words9 = new RegExp(owner);
     if (words9.test(document.querySelector("#input").value)) {
       getBotMessage.innerText = "Typing...";
@@ -178,9 +175,6 @@ function App() {
       }, 2000);
     }
 
-    let owner2 = [
-      "Who's Treasure|who's Treasure|Who is Treasure|who is Treasure",
-    ];
     let words10 = new RegExp(owner2);
     if (words10.test(document.querySelector("#input").value)) {
       getBotMessage.innerText = "Typing...";
@@ -191,9 +185,7 @@ function App() {
       }, 2000);
     }
 
-    let ageAsk = [
-      "What's your age|what's your age|What is your age|what is your age|How old are you|how old are you",
-    ]; //adding the age-question
+    //adding the age-question
     let words11 = new RegExp(ageAsk);
     if (words11.test(document.querySelector("#input").value)) {
       // if the input contains some question
@@ -210,7 +202,9 @@ function App() {
       <div className="wrapper">
         <div className="content">
           <div className="header">
-            <div className="img"><img src={image} alt="" /></div>
+            <div className="img">
+              <img src={image} alt="" />
+            </div>
             <div className="right">
               <div className="name">Hey There!!</div>
               <div className="status">Active</div>
